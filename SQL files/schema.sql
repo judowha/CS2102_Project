b7bbf3ee388b8ea320f2f4f7553517474f796473
judowha <e0376925@u.nus.edu>
2021-03-27 22:57:31 +0800

    change type float ot double precision correct some foreign key constrain error

--- SQL files/schema.sql
+++ SQL files/schema.sql
@@ -1,40 +1,3 @@
-create table Courses (
-  course_id     char(20) primary key,
-  title         text unique,
-  description   text,
-  duration      integer);
-
-create table Offerings (
-  launch_date                   date primary key,
-  fees                          double precision,
-  target_number_registrations   integer,
-  registration_deadline         timestamp,
-  seating_capacity              integer,
-  start_date                    date,
-  end_date                      date,
-  eid                           char(20) not null,
-  foreign key(eid) references Administrators);
-  
-create table Course_packages (
-  package_id                char(20) primary key,
-  price                     double precision,
-  num_free_registrations    integer,
-  name                      text,
-  sale_start_date           date,
-  sale_end_date             date);
-  
-create table Customers (
-  cust_id   char(20) primary key,
-  name      char(30),
-  phone     text,
-  email     text,
-  address   text);
-
-create table Credit_cards (
-  number        text primary key,
-  expiry_date   date,
-  CVV           integer);
-  
 create table Employees (
   eid           char(20) primary key,
   name          char(30),
@@ -73,6 +36,44 @@ create table Administrators (
 create table Managers (
   eid char(20) primary key,
   foreign key (eid) references Full_time_Emp on delete cascade);
+create table Courses (
+  course_id     char(20) primary key,
+  title         text unique,
+  description   text,
+  duration      integer);
+
+create table Offerings (
+  launch_date                   date primary key,
+  fees                          double precision,
+  target_number_registrations   integer,
+  registration_deadline         timestamp,
+  seating_capacity              integer,
+  start_date                    date,
+  end_date                      date,
+  eid                           char(20) not null,
+  foreign key(eid) references Administrators);
+  
+create table Course_packages (
+  package_id                char(20) primary key,
+  price                     double precision,
+  num_free_registrations    integer,
+  name                      text,
+  sale_start_date           date,
+  sale_end_date             date);
+  
+create table Customers (
+  cust_id   char(20) primary key,
+  name      char(30),
+  phone     text,
+  email     text,
+  address   text);
+
+create table Credit_cards (
+  number        text primary key,
+  expiry_date   date,
+  CVV           integer);
+  
+
 
 create table Pay_slips (
   payment_date      date,
@@ -109,40 +110,47 @@ create table Course_areas (
 create table Buys (
   buy_date   		              date,
   cust_id    		              char(20),
-  number     		              integer,
+  number     		              text,
   package_id  		            char(20), 
   num_remaining_redemptions   integer,
   foreign key (cust_id)    references Customers       on delete cascade on update cascade,
   foreign key (number)     references Credit_cards    on delete cascade on update cascade,
-  foreign key (package_id) references Course_packeges on delete cascade on update cascade,
+  foreign key (package_id) references Course_packages on delete cascade on update cascade,
   primary key (buy_date,cust_id, number, package_id));
 
 create table Redeems (
   redeem_date   date,
   cust_id       char(20),
-  number        integer,
+  number        text,
   package_id    char(20), 
-  sid           char(20),
+  session_id           char(20),
+  launch_date	 date,
+  course_id 	CHAR(20),
+  foreign key (session_id,launch_date,course_id)references Sessions        on delete cascade on update cascade,
   foreign key (cust_id)    references Customers       on delete cascade on update cascade,
   foreign key (number)     references Credit_cards    on delete cascade on update cascade,
-  foreign key (package_id) references Course_packeges on delete cascade on update cascade,
-  foreign key (sid)        references Sessions        on delete cascade on update cascade,
-  primary key (cust_id, number, package_id));
+  foreign key (package_id) references Course_packages on delete cascade on update cascade,
+  primary key (cust_id, number, package_id,session_id,launch_date,course_id));
 
 
 create table Conducts (
   room_id       char(20),
-  session_id    char(20),
-  primary key(room_id, session_id),
+  eid      		char(20),
+  session_id     char(20),
+  launch_date	 date,
+  course_id 	CHAR(20),
+  foreign key (session_id,launch_date,course_id)references Sessions  on delete cascade on update cascade,
   foreign key (room_id)    references Rooms    on delete cascade,
-  foreign key (session_id) references Sessions on delete cascade);
+  FOREIGN key (eid) REFERENCES instructors on delete cascade on update cascade
+);
+
 
 create table Specializes (
   eid     char(20),
   name    char(20),
   primary key(eid, name),
   foreign key (name) references Course_areas on delete cascade,
-  foreign key (eid)  references Employees    on delete cascade);
+  foreign key (eid)  references instructors    on delete cascade);
 
 create table Cours_in (
   name          char(20),
@@ -176,13 +184,13 @@ create table Owns(
 );
 
 create table Cancels(
-  sid char(20),
+  session_id char(20),
   course_id char(20),
   launch_date date,
   cust_id char(20),
   cancels_date date,
-  FOREIGN key (sid, course_id,launch_date) REFERENCES Sessions on DELETE CASCADE on UPDATE CASCADE,
+  FOREIGN key (session_id, launch_date,course_id) REFERENCES Sessions on DELETE CASCADE on UPDATE CASCADE,
   FOREIGN key (cust_id) REFERENCES Customers on DELETE CASCADE on UPDATE CASCADE,
-  PRIMARY key (sid, course_id, launch_date, cust_id, cancels_date)
+  PRIMARY key (session_id, course_id, launch_date, cust_id, cancels_date)
 );
 

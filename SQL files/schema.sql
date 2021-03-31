8dce6068e0392c3951604c8b8102a68102034f06
Zhu-Ze-Yu <61224431+Zhu-Ze-Yu@users.noreply.github.com>
2021-03-31 17:22:38 +0800

    modify3

--- SQL files/schema.sql
+++ SQL files/schema.sql
@@ -7,37 +7,45 @@ create table Employees (
   join_date     date,
   depart_date   date);
 
+
 create table Part_time_Emp (
-  eid char(20) primary key,
-  hourly_rate numeric,
+  eid char(20) 	primary key,
+  hourly_rate 	numeric,
   foreign key (eid) references Employees on delete cascade);
 
+
 create table Full_time_Emp (
-  eid char(20) primary key,
+  eid char(20) 	 primary key,
   monthly_salary numeric,
   foreign key (eid) references Employees on delete cascade);
+
 
 create table Instructors (
   eid char(20) primary key,
   foreign key (eid) references Employees on delete cascade);
 
+
 create table Part_time_instructors (
   eid char(20) primary key,
-  foreign key (eid) references Instructors on delete cascade,
+  foreign key (eid) references Instructors   on delete cascade,
   foreign key (eid) references Part_time_Emp on delete cascade);
+
 
 create table Full_time_instructors (
   eid char(20) primary key,
-  foreign key (eid) references Instructors on delete cascade,
+  foreign key (eid) references Instructors   on delete cascade,
   foreign key (eid) references Full_time_Emp on delete cascade);
+
 
 create table Administrators (
   eid char(20) primary key,
   foreign key (eid) references Full_time_Emp on delete cascade);
 
+
 create table Managers (
   eid char(20) primary key,
   foreign key (eid) references Full_time_Emp on delete cascade);
+  
   
 create table Courses (
   course_id     char(20) primary key,
@@ -45,9 +53,10 @@ create table Courses (
   description   text,
   duration      integer);
 
+
 create table Offerings (
   launch_date                   date,
-  course_id						char(20),
+  course_id			char(20),
   fees                          double precision,
   target_number_registrations   integer,
   registration_deadline         timestamp,
@@ -56,9 +65,9 @@ create table Offerings (
   end_date                      date,
   eid                           char(20) not null, 
   primary key (launch_date, course_id),
-  foreign key(eid) references Administrators on delete cascade,
-  foreign key(course_id) references courses on delete cascade
-);
+  foreign key(eid) 	 references Administrators on delete cascade,
+  foreign key(course_id) references courses 	   on delete cascade);
+  
   
 create table Course_packages (
   package_id                char(20) primary key,
@@ -67,7 +76,8 @@ create table Course_packages (
   name                      text,
   sale_start_date           date,
   sale_end_date             date);
-  
+ 
+ 
 create table Customers (
   cust_id   char(20) primary key,
   name      char(30),
@@ -75,11 +85,11 @@ create table Customers (
   email     text,
   address   text);
 
+
 create table Credit_cards (
   number        text primary key,
   expiry_date   date,
   CVV           integer);
-  
 
 
 create table Pay_slips (
@@ -91,65 +101,65 @@ create table Pay_slips (
   foreign key (eid) references Employees on delete cascade,
   primary key(payment_date, eid));
 
+	      
 create table Rooms (
   room_id		         char(20) primary key,
   location 		       text,
   seating_capacity   integer);
-
+	      
+	      
 create table Sessions (
-
-  session_id	char(20),
-  date 		    text,
-  start_time 	text,
-  end_time	    text,
+  sid		char(20),
+  date 		text,
+  start_time 	int,
+  end_time	int,
   launch_date   date,
   course_id     char(20),
-  foreign key (launch_date,course_id) references Offerings on delete cascade,
-  foreign key (course_id)   references Courses   on delete cascade,
-  primary key (session_id, launch_date, course_id));
+  foreign key (launch_date,course_id)	references Offerings on delete cascade,
+  foreign key (course_id)   		references Courses   on delete cascade,
+  primary key (sid, launch_date, course_id));
 
+	       
 create table Course_areas (
-
-  name      char(20) primary key
-);
+  name      char(20) primary key);
 
               
 create table Buys (
-  buy_date   		              date,
-  cust_id    		              char(20),
-  number     		              text,
-  package_id  		            char(20), 
-  num_remaining_redemptions   integer,
+  buy_date			date,
+  cust_id			char(20),
+  number			text,
+  package_id			char(20), 
+  num_remaining_redemptions	integer,
   foreign key (cust_id)    references Customers       on delete cascade on update cascade,
   foreign key (number)     references Credit_cards    on delete cascade on update cascade,
   foreign key (package_id) references Course_packages on delete cascade on update cascade,
   primary key (buy_date,cust_id, number, package_id));
 
+	       
 create table Redeems (
   redeem_date   date,
   cust_id       char(20),
   number        text,
   package_id    char(20), 
-  session_id           char(20),
-  launch_date	 date,
-  course_id 	CHAR(20),
-  foreign key (session_id,launch_date,course_id)references Sessions        on delete cascade on update cascade,
-  foreign key (cust_id)    references Customers       on delete cascade on update cascade,
-  foreign key (number)     references Credit_cards    on delete cascade on update cascade,
-  foreign key (package_id) references Course_packages on delete cascade on update cascade,
+  sid           char(20),
+  launch_date	date,
+  course_id 	char(20),
+  foreign key (sid,launch_date,course_id) references Sessions        on delete cascade on update cascade,
+  foreign key (cust_id)    		  references Customers       on delete cascade on update cascade,
+  foreign key (number)     		  references Credit_cards    on delete cascade on update cascade,
+  foreign key (package_id) 		  references Course_packages on delete cascade on update cascade,
   primary key (cust_id, number, package_id,session_id,launch_date,course_id));
 
 
 create table Conducts (
   room_id       char(20),
-  eid      		char(20),
-  session_id     char(20),
-  launch_date	 date,
-  course_id 	CHAR(20),
-  foreign key (session_id,launch_date,course_id)references Sessions  on delete cascade on update cascade,
-  foreign key (room_id)    references Rooms    on delete cascade,
-  FOREIGN key (eid) REFERENCES instructors on delete cascade on update cascade
-);
+  eid      	char(20),
+  sid     	char(20),
+  launch_date	date,
+  course_id 	char(20),
+  foreign key (sid,launch_date,course_id) references Sessions  	 on delete cascade on update cascade,
+  foreign key (room_id)    		  references Rooms    	 on delete cascade,
+  foreign key (eid) 			  references instructors on delete cascade on update cascade);
 
 
 create table Specializes (
@@ -157,54 +167,52 @@ create table Specializes (
   name    char(20),
   primary key(eid, name),
   foreign key (name) references Course_areas on delete cascade,
-  foreign key (eid)  references instructors    on delete cascade);
+  foreign key (eid)  references instructors  on delete cascade);
 
+	       
 create table Cours_in (
   name          char(20),
   course_id     char(20),
   primary key(course_id, name),
-  foreign key (name) references Course_areas on delete cascade,
-  foreign key (course_id) references Courses on delete cascade
-);
+  foreign key (name) 	  references Course_areas on delete cascade,
+  foreign key (course_id) references Courses 	  on delete cascade);
 
+	       
 create table Registers(
-  sid char(20),
-  course_id char(20),
-  launch_date date,
-  cust_id char(20),
-  number text,
+  sid 		 char(20),
+  course_id 	 char(20),
+  launch_date 	 date,
+  cust_id 	 char(20),
+  number 	 text,
   registers_date date,
-  FOREIGN key (sid,launch_date,course_id) REFERENCES Sessions on delete CASCADE on UPDATE CASCADE,
-  FOREIGN key (number)REFERENCES Credit_cards on delete CASCADE on UPDATE CASCADE,
-  FOREIGN key (cust_id)REFERENCES Customers on delete CASCADE on UPDATE CASCADE,
-  PRIMARY key (sid, course_id, launch_date,cust_id,number,registers_date)
+  foreign key (sid,launch_date,course_id) references Sessions 	  on delete cascade on update cascade,
+  foreign key (number)			  references Credit_cards on delete cascade on update cascade,
+  foreign key (cust_id)			  references Customers 	  on delete cascade on update cascade,
+  primary key (sid, course_id, launch_date,cust_id,number,registers_date));
 
-);
-
+	       
 create table Owns(
   number text,
   cust_id char(20) not null,
   from_date date,
-  FOREIGN key (number)REFERENCES Credit_cards on delete CASCADE on UPDATE CASCADE,
-  FOREIGN key (cust_id)REFERENCES Customers on delete CASCADE on UPDATE CASCADE,
-  PRIMARY key (number)
-);
+  foreign key (number) references Credit_cards on delete cascade on update cascade,
+  foreign key (cust_id)references Customers    on delete cascade on update cascade,
+  primary key (number));
+	       
 
 create table Cancels(
-  session_id char(20),
-  course_id char(20),
-  launch_date date,
-  cust_id char(20),
-  cancels_date date,
-  FOREIGN key (session_id, launch_date,course_id) REFERENCES Sessions on DELETE CASCADE on UPDATE CASCADE,
-  FOREIGN key (cust_id) REFERENCES Customers on DELETE CASCADE on UPDATE CASCADE,
-  PRIMARY key (session_id, course_id, launch_date, cust_id, cancels_date)
-);
+  sid 		char(20),
+  course_id 	char(20),
+  launch_date 	date,
+  cust_id 	char(20),
+  cancels_date 	date,
+  foreign key (sid, launch_date,course_id) references Sessions 	on delete cascade on update cascade,
+  foreign key (cust_id) 		   references Customers on delete cascade on update cascade,
+  primary key (session_id, course_id, launch_date, cust_id, cancels_date));
+	       
 
 create table Manage(
 	name char(20) primary key,
-	eid text,
+	eid 	      text,
 	foreign key (name) references course_areas on delete cascade on update cascade,
-	foreign key (eid) references managers on delete cascade on update cascade
-);
-
+	foreign key (eid)  references managers 	   on delete cascade on update cascade);
